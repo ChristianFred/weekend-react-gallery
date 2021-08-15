0849caa11af18882a57259b67d3e97f81e575290
Christian Fredricks <Christianpfred@gmail.com>
2021-08-15 16:58:29 -0500

    added GET to retrieve date from /gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+
+let [galleryList, setGalleryList] = useState([]);
+
+useEffect(() => {
+  getImages();
+});
+
+const getImages = () => {
+  axios
+    .get('/gallery')
+    .then((response) => {
+      setGalleryList(response.data);
+    })
+    .catch((error) => {
+      alert('Could not get the gallery.');
+      console.log('Error getting galleryList', error);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
       <div className="App">
         <header className="App-header">
